7585c079d8472b2bdbe24cb3712a38bb66feee0d
MohammadTaha1388 <mhmdtahaafdlygrwh2@gmail.com>
2026-07-14 21:36:58 +0330

    Create context.py

---
+++ core/context.py
@@ -0,0 +1,14 @@
+class Context:
+
+    def __init__(self):
+        self.data = {}
+
+    def set(self, key, value):
+        self.data[key] = value
+
+    def get(self, key):
+        return self.data.get(key)
+
+    def remove(self, key):
+        if key in self.data:
+            del self.data[key]
