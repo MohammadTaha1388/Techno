a396ec78f9712839e3cb56273e045ed17d4295b6
MohammadTaha1388 <mhmdtahaafdlygrwh2@gmail.com>
2026-07-14 21:34:46 +0330

    Create planner.py

---
+++ core/planner.py
@@ -0,0 +1,13 @@
+class Planner:
+
+    def create_plan(self, goal):
+
+        return {
+            "goal": goal,
+            "steps": [
+                "تحلیل هدف",
+                "انتخاب ابزار",
+                "اجرا",
+                "بررسی نتیجه"
+            ]
+        }
